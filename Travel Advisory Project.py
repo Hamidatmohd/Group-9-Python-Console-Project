f451c050569eb45af36e4ae6f4d78c1fa4930a0b
olajumoke001 <69039712+olajumoke001@users.noreply.github.com>
2024-01-08 19:13:29 +0100

    Update Travel Advisory Project.py

--- Travel Advisory Project.py
+++ Travel Advisory Project.py
@@ -17,7 +17,18 @@ class tripInformation:
     countryUrl = 'https://restcountries.com/v3.1/name/{name}'.format(name = formatCountryName) #endpoint
     response = requests.get(countryUrl) #making a call to the api
     checkConnection = response.status_code  #gets the status code and stores it to a variable
-
+    if checkConnection != 200:
+      print('error!, country name is invalid or spelled incorrectly, check again') # Print error message if the request failed
+    else:
+      countryData = response.json() # Retrieve country data if the request was successful
+      with open('countries.txt', 'w') as file: # Open a text file to write the country information
+        file.write('These are some of the basic facts on the country:'+'\n'+ '\n'+ '\n') # write header
+        #Iterate through each country in the response data
+        for i in countryData:
+          file.write('Name: ' + str(i.get('name')) + '\n') # Write the country's name to the file and creates a new line
+          file.write('Languages: ' + str(i.get('languages')) + '\n') # Write the country's languages to the file
+          file.write('Currencies: ' + str(i.get('currencies')) + '\n') # Write the country's currencies to the file
+          file.write('Alt Spellings: ' + str(i.get('altSpellings')) + '\n\n') # Write the country's alternative spellings to the file and creates two new lines in the file
     #api call for getting latest news from countries
   #api key is the same for all free calls
   #apikey= 9e3bd8f42a84440a86fe64d1d56391d4
